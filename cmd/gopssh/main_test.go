932a86efd4cbd16e4b3bd61529230f8c8ad848e4
YAMASAKI Masahide <masahide.y@gmail.com>
2018-10-15 13:14:15 +0900

    add test

--- cmd/gopssh/main_test.go
+++ cmd/gopssh/main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"os"
 	"testing"
 	"time"
 )
@@ -27,5 +30,30 @@ func TestNewConfig(t *testing.T) {
 	}
 	if c.Timeout != 5*time.Second {
 		t.Error("c.Timeout != 5*time.Second")
+	}
+}
+
+func TestCheckFlag(t *testing.T) {
+	var tests = []struct {
+		args     []string
+		showVer  bool
+		wantRet  int
+		wantExit bool
+	}{
+		{args: []string{"1", "2"}, showVer: false, wantRet: 0, wantExit: false},
+		{args: []string{"1"}, showVer: false, wantRet: 2, wantExit: false},
+		{args: []string{"1"}, showVer: true, wantRet: 0, wantExit: true},
+	}
+	for i, test := range tests {
+		os.Args = test.args
+		flag.Parse()
+		showVer = &test.showVer
+		b := []byte{}
+		buf := bytes.NewBuffer(b)
+		if ret, exit := checkFlag(buf); exit {
+			if ret != test.wantRet {
+				t.Errorf("%d ret=%d,wantRet:%d", i, ret, test.wantRet)
+			}
+		}
 	}
 }
