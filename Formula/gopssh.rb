41b925b8a73415186de280d4a223812df90e71d8
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-09 21:33:04 +0900

    Brew formula update for gopssh version v0.4.0

--- Formula/gopssh.rb
+++ Formula/gopssh.rb
@@ -1,9 +1,9 @@
 class Gopssh < Formula
   desc "parallel ssh client"
   homepage "https://github.com/masahide/gopssh"
-  url "https://github.com/masahide/gopssh/releases/download/v0.3.0/gopssh_Darwin_x86_64.tar.gz"
-  version "0.3.0"
-  sha256 "9cc7b442406bc35e9153d32db73cf798346b304cc81541eecb7bf94c034a633a"
+  url "https://github.com/masahide/gopssh/releases/download/v0.4.0/gopssh_Darwin_x86_64.tar.gz"
+  version "0.4.0"
+  sha256 "a86f4d76e84aa3858283611dea3c6679e5b2ed30bc9e7fcdef7e56c7e2f72f05"
 
   def install
     bin.install "gopssh"
