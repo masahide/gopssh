a7aa6b9dc1ab97cd181092a283e2a1cc11918f4b
YAMASAKI Masahide <masahide.y@gmail.com>
2018-12-16 21:51:15 +0900

    fix show version

--- Formula/gopssh.rb
+++ Formula/gopssh.rb
@@ -1,9 +1,9 @@
 class Gopssh < Formula
   desc "parallel ssh client"
   homepage "https://github.com/masahide/gopssh"
-  url "https://github.com/masahide/gopssh/releases/download/v0.4.0/gopssh_Darwin_x86_64.tar.gz"
-  version "0.4.0"
-  sha256 "a86f4d76e84aa3858283611dea3c6679e5b2ed30bc9e7fcdef7e56c7e2f72f05"
+  url "https://github.com/masahide/gopssh/releases/download/v0.5.0/gopssh_Darwin_x86_64.tar.gz"
+  version "0.5.0"
+  sha256 "7f21a4b1c562a3823095e98639e0bd256c0ccf37f611e09f8a6285f3faaf0788"
 
   def install
     bin.install "gopssh"
